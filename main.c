9b51670e368315163440a7c521f2092653b7949a
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 22:59:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,40 +3,46 @@
 
 int main()
 {
-    int i,n=7,sonuc,j,son,array[]={6,14,43,53,64,32,24};
+    int i, n = 7, sonuc, j, son, array[] = {6, 14, 43, 53, 64, 32, 24};
 
-    sonuc=1;
+    sonuc = 1;
 
-    son=0;
+    son = 0;
 
-    printf("ba?langiç dizisi: ");
+    printf("ba?langiÃ§ dizisi: ");
 
-    for(i=0;i<n;i++){
-        printf("%d ",array[i]);
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
     }
 
-    for(i=0;i<n;i++){
-        if(array[i]%2==0){
-            array[i]=0;
+    for (i = 0; i < n; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            array[i] = 0;
         }
-        else{
-            array[i]=1;
+        else
+        {
+            array[i] = 1;
         }
     }
     printf("\n parite dizisi: ");
 
-    for(i=0;i<n;i++){
-        printf("%d ",array[i]);
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
     }
 
-    for(i=0;i<n;i++){
-        for(j=n-1;j>=0;j--){
-            sonuc*=2;
+    for (i = 0; i < n; i++)
+    {
+        for (j = n - 1; j >= 0; j--)
+        {
+            sonuc *= 2;
         }
-        son+=array[i]*sonuc;
+        son += array[i] * sonuc;
     }
-    printf("\n guvenlik kodu: %d \n",son);
+    printf("\n guvenlik kodu: %d \n", son);
 
     return 0;
 }
-
